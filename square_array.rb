cfb38a8d57e69126d04d8ee41dd0277ffdfe0e56
rtyenriques <tooshopboys@gmail.com>
2021-01-16 21:12:17 -0800

    commit tree f3fb9419d9ed565eb8cc3b564d8d6afbc626f99c

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
-new_array = []
-array.each do |numbers|
-  new_array << numbers ** 2
-end
-return new_array
-end
+  new_array = []
+  array.each do |numbers|
+    new_array << numbers ** 2
+  end
+  new_array
+  end
